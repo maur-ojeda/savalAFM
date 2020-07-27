30d6635c4b5002c349733b79c90be928403854ce
Your global username <your@email.com>
2020-07-27 15:18:58 -0400

    pre npm install --save @ng-bootstrap/ng-bootstrap

--- src/app/pages/fixed-assets/fixed-assets.component.ts
+++ src/app/pages/fixed-assets/fixed-assets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AssetsService } from '../../services/assets.service';
 import { AssetInterface } from '../../interfaces/asset.interface';
 
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { RequestInterface } from 'src/app/interfaces/request.interface';
 import { RequestsService } from 'src/app/services/requests.service';
 
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap' 
 
 
 @Component({
@@ -20,26 +21,16 @@ import { RequestsService } from 'src/app/services/requests.service';
 
 export class FixedAssetsComponent implements OnInit {
 
-
-  elements: any = [];
-  headElements = ['id', 'first', 'last', 'handle'];
-
-  //val: string = ''; 
-
   assets: AssetInterface[] = [];
   requests: RequestInterface[] = [];
 
   constructor(
     private assetsService: AssetsService,
     private requestsService: RequestsService,
+    private modal:NgbModal,
     private router: Router) { }
 
   ngOnInit(): void {
-
-
-    for (let i = 1; i <= 15; i++) {
-      this.elements.push({ id: i, first: 'User ' + i, last: 'Name ' + i, handle: 'Handle ' + i });
-    }
 
 
     
