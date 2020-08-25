cee131eac77b3e3c21ef46e4399ffc217920d643
Your global username <your@email.com>
2020-08-25 19:44:26 -0400

    update template

--- src/app/pages/asset-delete/asset-delete.component.ts
+++ src/app/pages/asset-delete/asset-delete.component.ts
@@ -4,7 +4,9 @@ import { SharedserviceService} from '../../services/sharedservice.service';
 import { AssetsDeleteService } from '../../services/assets-delete.service';
 import { Asset } from 'src/app/models/asset.model';
 import { Observable } from 'rxjs';
-
+import {MatDialog} from '@angular/material/dialog';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DeleteConfirmationComponent } from 'src/app/dialogs/delete-confirmation/delete-confirmation.component';
 
 @Component({
   selector: 'app-asset-delete',
@@ -13,17 +15,64 @@ import { Observable } from 'rxjs';
 })
 export class AssetDeleteComponent implements OnInit {
   public asset$:Observable<Asset[]>;
-  
+  reactiveForm: FormGroup;
 
   constructor(
     public assetsDeleteService: AssetsDeleteService,
     private activatedRoute: ActivatedRoute,
-    public utils: SharedserviceService
+    public utils: SharedserviceService,
+    private builder: FormBuilder,
+    public dialog: MatDialog 
   ) { }
 
   ngOnInit(): void {
       let code = this.activatedRoute.snapshot.paramMap.get('id');
       this.asset$ = this.assetsDeleteService.findByCode(code);
+   
+   
+      this.reactiveForm = this.builder.group({
+        assetID:['',[]],
+        downDocumentAt: ['',[Validators.required]],
+        downPostingAt: ['', [Validators.required]],
+        downReferenceAt: ['', [Validators.required]],
+        downComment: ['', [Validators.required]],
+      });
     }
+
+
+
+    deleteAsset(){
+
+      let ide = this.reactiveForm.value.assetID;
+  
+      let formValue = {
+        "downDocumentAt": this.reactiveForm.value.downDocumentAt,
+        "downPostingAt": this.reactiveForm.value.downPostingAt,
+        "downReferenceAt": this.reactiveForm.value.downReferenceAt,
+        "downComment": this.reactiveForm.value.downComment
+      }
+  
+      console.log(JSON.stringify(formValue));
+      console.log(JSON.stringify(ide));
+      // Buscar el metodo en base service
+      // this.assetsService.downAssets(formValue, ide);
+    }
+
+
+    Dialog() {
+      const dialogRef = this.dialog.open(DeleteConfirmationComponent,{
+            width: '98VW'
+      });
+      
+      dialogRef.afterClosed().subscribe(result => {
+        //console.log(`Dialog result: ${result}`);
+                if(result){
+                  this.deleteAsset();
+                }
+  
+      });
+    }
+
+
   }
 
--- src/app/pages/asset-update/asset-update.component.ts
+++ src/app/pages/asset-update/asset-update.component.ts
@@ -4,6 +4,12 @@ import { SharedserviceService} from '../../services/sharedservice.service';
 import { AssetsUpdateService } from '../../services/assets-Update.service';
 import { Asset } from 'src/app/models/asset.model';
 import { Observable } from 'rxjs';
+import {MatDialog} from '@angular/material/dialog';
+import { WarningComponent } from 'src/app/dialogs/warning/warning.component';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CcenterService } from 'src/app/services/ccenter.service';
+import { CcenterInterface } from 'src/app/interfaces/ccenter.interface';
+import { UpdateConfirmationComponent } from 'src/app/dialogs/update-confirmation/update-confirmation.component';
 
 
 @Component({
@@ -13,16 +19,86 @@ import { Observable } from 'rxjs';
 })
 export class AssetUpdateComponent implements OnInit {
   public asset$:Observable<Asset[]>;
+  reactiveForm: FormGroup;
+  ccenters: CcenterInterface[] = [];
 
   constructor(
+    public slCCenterService: CcenterService,
     public assetsUpdateService: AssetsUpdateService,
     private activatedRoute: ActivatedRoute,
-    public utils: SharedserviceService
+    public utils: SharedserviceService,
+    private builder: FormBuilder,
+    public dialog: MatDialog 
   ) { }
 
   ngOnInit(): void {
     let code = this.activatedRoute.snapshot.paramMap.get('id');
     this.asset$ = this.assetsUpdateService.findByCode(code);
+    this.asset$.subscribe(
+      (asset)=>{this.getAssetsData(asset) }
+    )
+
+
+    this.slCCenterService.getCcenters()
+      .then(ccenters => this.ccenters = ccenters);
+
+    this.reactiveForm = this.builder.group({
+      assetID: ['', []],
+      rfidLabelSap: ['',[] ],
+      serieNumber: ['', []],
+      description: ['', [Validators.required]],
+      costCenter: ['', [Validators.required]],
+      creditorId: ['', []],
+      lifetimeYear: ['', []]
+    });
+
   }
+
+
+
+  getAssetsData(i) {
+   let e = i['data'];
+    console.log(e)
+    this.reactiveForm.controls['assetID'].setValue(e.id);
+    this.reactiveForm.controls['rfidLabelSap'].setValue(e.rfidLabelSap);
+    this.reactiveForm.controls['costCenter'].setValue(e.costCenter.id);
+    this.reactiveForm.controls['serieNumber'].setValue(e.serieNumber);
+    this.reactiveForm.controls['description'].setValue(e.description);
+    this.reactiveForm.controls['creditorId'].setValue(e.creditorId);
+    this.reactiveForm.controls['lifetimeYear'].setValue(e.lifetimeYear);
+  }
+
+  updateData() {
+    let ide = this.reactiveForm.value.assetID;
+    let formValue = {
+      "rfidLabelSap": this.reactiveForm.value.rfidLabelSap,
+      "serieNumber": this.reactiveForm.value.serieNumber,
+      "description": this.reactiveForm.value.description,
+      "costCenter": this.reactiveForm.value.costCenter,
+      "creditorId": this.reactiveForm.value.creditorId,
+      "lifetimeYear": this.reactiveForm.value.lifetimeYear
+    }
+
+   console.log(JSON.stringify(formValue))
+   console.log(JSON.stringify(ide))
+    //this.assetsService.updateAssets(formValue, ide);
+   
+  }
+
+
+
+  Dialog() {
+    const dialogRef = this.dialog.open(UpdateConfirmationComponent,{
+          width: '98VW'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+
+              if(result){
+             this.updateData();
+              }
+
+    });
+  }
+
 
 }
